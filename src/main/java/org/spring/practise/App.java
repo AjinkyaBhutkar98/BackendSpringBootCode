7492297cecc9bc9d1df8df6f3e15172dafa16fcc
Ajinkya Bhutkar <bhutkar16@gmail.com>
2025-05-01 18:38:35 +0530

    spring di object creation with application context

--- src/main/java/org/spring/practise/App.java
+++ src/main/java/org/spring/practise/App.java
@@ -1,5 +1,8 @@
 package org.spring.practise;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+      Task t=new Task("Code Review");
+      Employee e1=new Employee(1,"Ajinkya Bhutkar",t);
+
+        e1.assignTask();
+
+        ApplicationContext context=new ClassPathXmlApplicationContext("config.xml");
+
+        Employee e2=context.getBean("emp1",Employee.class);
+
+        e2.assignTask();
     }
 }
