b6102d41763470cb6b0eba4d5df392dd37d33cd9
Ajinkya Bhutkar <bhutkar16@gmail.com>
2025-05-01 18:14:55 +0530

    addition of two classes employee and task and simple object creation in java

--- src/main/java/org/spring/practise/App.java
+++ src/main/java/org/spring/practise/App.java
@@ -9,5 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        Task t=new Task("Code Review");
+        Employee e1=new Employee(1,"Ajinkya Bhutkar",t);
+
+        t.assignTask();
+
     }
 }
